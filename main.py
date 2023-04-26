11d4f3d8a767f25a7048d2da1488924d54b3be55
Ohiani1 <78965391+Ohiani1@users.noreply.github.com>
2023-04-26 11:38:53 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,35 @@
+import praw
+
+reddit = praw.Reddit(
+    client_id="goCW4r2J6JdPhiQDLdS5zg",
+    client_secret="JIPIj30b3RuWNBVekK5aXJz-IKKCWg",
+    user_agent="reddit bot",
+    username="Alternative_Metal_89",
+    password="Junior1235",
+)
+
+import time
+users = set()
+
+def keyword():
+  for comment in reddit.subreddit("dropshipping+dropship+Dropshipping_Guide+Dropshipping101+Dropshipsupplier+DropshippingService").streeam.comments():
+    try:
+      name = comment.author.name
+      title = f"Hello, {name}"
+      text = "You asked and we answered! As expereinced dropshippers, we know selling saturated products can eat into your profits.\n\nTo combat this problem, the Competiton Meter team has created a tool that helps you know if a product is untapped, competitive, or saturated! \n\nTo top it all off, we added a feature to the tool that helps users find the cheapest option for any product they want to sell. This will help you minimize your business expenses and increase your profit potential!! \n\nThe tool is a google chrome extension. If you would like to learn more about it, check out our website => https://competitionmeter.com/"
+
+      if name in users:
+        continue
+      else:
+        try:
+          users.add(name)
+          reddit.redditor(name).message(title, text)
+        except:
+          continue
+      print(f"u/{name}, comment body: {comment.body}")
+      time.sleep(40)
+    except:
+      time.sleep(300)
+      keyword()
+      
+keyword()
